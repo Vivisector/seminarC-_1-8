0a6d7bb3a999343ec4a33e7d6b9b93143710225c
Dmitry Beliakov <hulk295@yandex.ru>
2022-11-30 19:30:28 +0300

    решена задача 8 из ДЗ

---
+++ task8/Program.cs
@@ -0,0 +1,9 @@
+﻿// выходе показывает все чётные числа от 1 до введенного N
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+string sN = "";
+for (int i = 2; i <= N; i = i + 2)
+{
+    sN = sN + " " + Convert.ToString(i);
+Console.WriteLine(sN);
