68752863c3c1055557563f26e4c3a9b1a38f5ed7
Dmitry Beliakov <hulk295@yandex.ru>
2022-11-30 18:53:58 +0300

    решана задача 2

---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите первое число:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n2 > n1 ? "Второе число больше" : "Первое число больше");
