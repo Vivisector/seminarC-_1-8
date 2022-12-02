2b8df233a19f3f9be7dbc2a897be7db639a1f414
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-02 23:16:27 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// генерируем сл. трехзначное число, и превращаем в двухзначное
+int n1 = new Random().Next(100,1000);
+int n01 = n1/100*10;
+// Console.WriteLine(n01);
+int n02 = n1 % 10;
+Console.WriteLine("Сгенерировано " + n1);
+Console.WriteLine($"Превратилось в {n01+n02}");
+
