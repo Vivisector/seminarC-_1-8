40cda05fc14e3b36211ac3ff91057ecd2b459b6e
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-02 23:29:32 +0300

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,10 @@
+﻿// ghdthrf ddtlyyjuj xbckf yf rhfnyjcnm 7 b 23-v
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0 && n % 23 == 0)
+{
+    Console.WriteLine("Число кратно семерке и 23-м");
+}
+else{
+    Console.WriteLine("Число не кратно 7 и 23");
+}
