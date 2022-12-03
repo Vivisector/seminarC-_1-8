61de735c7525ecc57c5e00ab90f02ff4f144491c
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-03 13:39:10 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите цифру, обоначающую день недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+// проверка введенной цифры на валидность
+if (day < 1 || day > 7){
+    Console.WriteLine("Такого дня не существует. Введите еще раз:");
+    day = Convert.ToInt32(Console.ReadLine());
+}
+if (day == 6 || day == 7){
+    Console.WriteLine("Это выходной день");
+} else{
+    Console.WriteLine("Рабочий день");
+}
