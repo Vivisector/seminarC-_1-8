90cff66093d572cbe29042affb98c6a4433e6733
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-03 13:39:50 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Из трёхзначного числа показывается вторая цифра
+
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = (n / 10) % 10;
+// int n2 = n1 % 10;
+Console.WriteLine($"Средняя цифра из числа {n} будет {n1}");
+
+
