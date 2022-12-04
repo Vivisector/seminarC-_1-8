c8fe549cd3df38b915f84c344419bdb2894ea45b
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-04 12:53:49 +0300

    под подсказке преподавателя сделал укороченный (более красивый) вариант поиска третьей цифры введенного пользователем числа

---
+++ task13-1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Придумайте число:");
+int n0 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Это число {n0}");
+
+while (n0 < 100) {
+    Console.WriteLine("Оно содержит меньше трех разрядов, введите другое число:");
+    n0 = Convert.ToInt32(Console.ReadLine());
+}
+
+int i = 0;
+int n1 = n0;
+while (n1 > 999)
+{
+    n1 = n1 / 10;
+    i++;
+}
+
+    Console.WriteLine($"Третья цифра {n1 % 10}");
