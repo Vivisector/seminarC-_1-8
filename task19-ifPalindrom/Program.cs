48643acace928f1c78ccd1e39394a140965fda55
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-07 17:58:28 +0300

    task19-ifPalindrom

---
+++ task19-ifPalindrom/Program.cs
@@ -0,0 +1,23 @@
+﻿// проверяем пятизначное число, является ли оно палиндромом.
+// 
+Console.WriteLine("Введите пятизначное число");
+int s = Convert.ToInt32(Console.ReadLine());
+// проверка валидности введенных данных
+if (s < 10000 || s > 99999){
+    Console.WriteLine("неверное число разрядов, повторите ввод:");
+    s = Convert.ToInt32(Console.ReadLine());
+}
+// Console.WriteLine($"первая цифра {s/10000}");
+// Console.WriteLine($"пятая цифра {s%10}");
+// Console.WriteLine($"вторая цифра {s/1000%10}");
+// Console.WriteLine($"четвертая цифра {s%100/10}");
+
+    if (s/10000!=s%10) {
+        Console.WriteLine($"Число не палиндром");
+    }
+    else if (s/1000%10!=s%100/10) {
+        Console.WriteLine($"Число не палиндром");
+    }
+    else {
+        Console.WriteLine($"число {s} - палиндром");
+    }
