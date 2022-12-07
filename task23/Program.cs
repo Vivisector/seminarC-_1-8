3afeac8dffb88bd34ba3db641f6e95255b4918c7
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-07 21:37:46 +0300

    task23 - кубы чисел

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Придумайте число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Составляющие числа (в скобках указан куб каждого числа):");
+for (int i = 1; i <= N; i++)
+{
+    if (i==N){
+        Console.Write($"{i}({Math.Pow(i, 3)})."); //обработка последней цифры: с точкой в конце
+    }
+        else{
+        Console.Write($"{i}({Math.Pow(i, 3)}), ");
+    }
+}
+
