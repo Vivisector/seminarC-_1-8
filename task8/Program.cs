55b4b283d737e0b5d432d096115545b63a2abb19
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-09 22:40:05 +0300

    подчищен код

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,7 +1,6 @@
-﻿// выходе показывает все чётные числа от 1 до введенного N
+﻿// показывает все чётные числа от 1 до введенного N
 Console.WriteLine("Введите число N:");
 int N = Convert.ToInt32(Console.ReadLine());
-int n = 0;
 string sN = "";
 for (int i = 2; i <= N; i = i + 2)
 {
