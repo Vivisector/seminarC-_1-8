99490ffc9085054e602c6514be79813b49ff576b
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-11 23:12:37 +0300

    досохранение task20

---
+++ task20/Program.cs
@@ -0,0 +1,15 @@
+﻿// AB = Корень: (xb-xa)2 + (yb-ya)2
+// int a = 25, b = 5;
+Console.WriteLine("Введите координаты точки А (x, y) через пробел:");
+// надо делать проверку на отсутствие литералов (вроде так называется)
+string[] valA = Console.ReadLine().Split();
+int xa = Convert.ToInt32(valA[0]);
+int ya = Convert.ToInt32(valA[1]);
+string[] valB = Console.ReadLine().Split();
+Console.WriteLine("Введите координаты точки B (x, y) через пробел:");
+int xb = Convert.ToInt32(valB[0]);
+int yb = Convert.ToInt32(valB[1]);
+
+// double resQ = Math.Sqrt(a);
+// double resP = Math.Pow(a, b); //a^b
+Console.WriteLine($"Расстояние между точками A и B + (на плоскости) согласно формуле составит: {Math.Sqrt((xb-xa)*(xb-xa) + (yb-ya)*(yb-ya))}");
