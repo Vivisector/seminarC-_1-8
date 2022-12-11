4a007677826f3d8e54e32a7989043225a7b7a649
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-11 19:53:24 +0300

    сгенерирован и показан массив из 8 случайных 0 и 1

---
+++ task30/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int[] arr = new int[8];
+
+void FillArr(int[] arr) //заполнитель массива
+{
+    for (int k = 0; k < arr.Length; k++)
+    {
+        arr[k] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArr(int[] arr) //распечатка на экране
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if(i==0)
+           {Console.Write($"[{arr[i]}, ");
+            }else if (i== arr.Length-1){
+            Console.Write($"{arr[i]}]");
+            }else{
+         Console.Write($"{arr[i]}, ");
+            }
+    }
+}
+FillArr(arr);
+PrintArr(arr);
