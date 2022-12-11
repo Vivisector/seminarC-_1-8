e90e39d19b75927a68abd72f91c2f065cbf19c52
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-11 19:56:16 +0300

    выводятся квадраты всех составляющих чисел введенного числа

---
+++ task22/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число для вывода квадратов всех чисел");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+// int s = 0;
+Console.WriteLine("Квадраты входящих чисел:");
+for (int i=1; i<=n; i++){
+a = i;
+a = a*a;
+    if (i==n){
+    Console.Write($"{i}^2 = {a}.");
+    }
+    else{
+    Console.Write($"{i}^2 = {a}, ");
+    }
+}
+// Console.WriteLine(s);
