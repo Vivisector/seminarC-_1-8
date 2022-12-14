185400e24c71e02c4e826e94da99729ffddb9ba4
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-14 17:40:57 +0300

    task33

---
+++ task33/Program.cs
@@ -0,0 +1,35 @@
+﻿// Создать массив и определить, присутствует ли в нем заданное число
+int A = 4;
+int B = -3;
+int[] arr = new int[7];
+
+void FillArr(int[] arr){
+    for (int i=0; i<arr.Length; i++){
+        arr[i] = new Random().Next(-5,5);
+    }
+}
+
+void ReadArr(int[] arr){
+    for (int i=0; i<arr.Length;i++){
+        Console.Write(arr[i] + ", ");    
+
+    }
+}
+FillArr(arr);
+Console.Write("Полученный массив:\n");    
+ReadArr(arr);
+Console.WriteLine();
+// проверка наличия заданного числа внутри массива
+bool mIn = false;
+for (int i=0;i<arr.Length; i++){
+    if (arr[i]==A){
+        Console.WriteLine($"Число {A} входит в массив на позиции {i}\n");
+        mIn = true;
+    }
+    if (arr[i]==B){
+        Console.WriteLine($"Число {B} входит в массив на позиции {i}\n");
+        mIn = true;
+    }
+}
+if (mIn == false) Console.WriteLine(" Ни {B} ни {A} не входят в массив.");;
+// Console.WriteLine("Hello, World!");
