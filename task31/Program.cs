195011b296a12f3daf5ea5abe3ecdf1c74b25f43
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-14 17:21:00 +0300

    task31+task32

---
+++ task31/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задать массив из 12 элементов, случайные числа из [-9 - 9]. Найти сумму отриц и пложит элементов
+// квадратные пустые скобки в инициализации переменной говорит, что у нас будет возвращаться на одно число, а набор чисел
+int[] arr = new int[12];
+int mPlus = 0;
+int mMinus = 0;
+
+void FillArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9,9);
+    }
+
+}
+void ReadArr(int[] arr){
+for (int k=0; k<arr.Length; k++){
+    Console.Write(arr[k] + ", ");
+}
+}
+FillArr(arr);
+Console.WriteLine("В полученном массиве:");
+ReadArr(arr);
+Console.WriteLine();
+Console.WriteLine("сумма плюсов и минусов следующая:");
+// подсчет 
+for (int i= 0; i<arr.Length; i++){
+    if (arr[i]>0) {
+        mPlus = mPlus + arr[i];
+    }else{
+        mMinus = mMinus + arr[i];
+    }
+}
+//подсчет минусов
+Console.WriteLine("+: " + mPlus);
+Console.WriteLine("-: " + mMinus);
+
+
+// Console.WriteLine("Hello, World!");
---
+++ task32/Program.cs
@@ -0,0 +1,30 @@
+﻿// Програма замены элементов массива: положительные элементы заменить на соответсвующие отрицательные и наоборот
+// [-4,-8,8,2] -> [4,8,-,8,-2]
+int Len = new Random().Next(5, 10);
+int[] arr = new int[Len];
+void FillArr(int[] arr, int Len)
+{
+
+    for (int i = 0; i < Len; i++)
+    {
+        arr[i] = new Random().Next(-9, 9);
+    }
+}
+void ReadArr(int[] arr, int Len)
+{
+    for (int k = 0; k < Len; k++)
+    {
+        Console.Write(arr[k] + ", ");
+    }
+}
+FillArr(arr, Len);
+Console.WriteLine("Исходный массив:");
+ReadArr(arr, Len);
+Console.WriteLine();
+// смена знаков
+for (int i=0; i<Len;i++){
+    arr[i]=-arr[i];
+}
+Console.WriteLine("Измененный массив:");
+ReadArr(arr, Len);
+// Console.WriteLine("Hello, World!");
