3b03aa3c2f7472e5e8ca3283fc328e6c9339a07d
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-15 21:58:00 +0300

    task34 - подсчет кол-ва четных элементов в случайном массиве

---
+++ task34/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int len = new Random().Next(5, 10);
+
+int[] GetRandArray(int len, int left, int right)
+{
+    int[] arr = new int[len];
+    Random rand = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rand.Next(left, right);
+    }
+    return arr;
+}
+const int LEFTN = 100; int RIGHTN = 1000;
+int[] mArr = GetRandArray(len, LEFTN, RIGHTN);
+Console.Write($"Исходный массив: {string.Join(", ", mArr)}");
+int cntEven = 0; //счетчик четных элементов
+    for (int k = 0; k < len; k++)
+    {
+        if (mArr[k] % 2 == 0) cntEven = cntEven + 1;
+    }
+Console.Write($"\nЧетных элементов в массиве: {cntEven}");
