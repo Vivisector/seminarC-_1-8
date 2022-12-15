be188ab6908d18c9eefdc238efb53df53be4d72b
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-15 23:33:45 +0300

    task36 подсчиталась сумма всех нечетных элементов

---
+++ task36/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+const int LEN = 7; const int L = -100; const int R = 100;
+
+int[] GetRandArr(int LEN, int L, int R){
+    int[] arr = new int[LEN];
+    Random randN = new Random();
+    for (int i=0; i< LEN; i++){
+        arr[i] = randN.Next(L, R);
+    }
+    return arr;
+}
+
+int[] mass = GetRandArr(LEN, L, R); // собрали массив из сл. чисел
+Console.WriteLine($"Исходный массив: {string.Join(", ", mass)}"); //посмотрели
+
+// считалка нечетных элементов
+int GetCount(int[] arr){
+int oddSum = 0;
+    for (int i=0;i<arr.Length; i++){
+        if (i%2!=0) oddSum+= arr[i];
+    }
+return oddSum;
+}
+Console.Write($"Сумма нечетных элементов составит {GetCount(mass)} шт."); //отчитались
