e4d2b64daeaf88c505390cca92e92cbce844a3ee
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-19 22:06:30 +0300

    получается пока только через разбор введенной строки, что не совсем по заданию

--- task41skolkbolshe/Program.cs
+++ task41skolkbolshe/Program.cs
@@ -1,21 +1,26 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода слова "stop"
 Console.WriteLine("Введите числа, разделяя ввод клавишей пробел (для прекращения ввода напишите 'stop'):");
 
-            string chisla = Console.ReadLine();
+string chisla = Console.ReadLine();
 
-            string[] chArr = chisla.Split();
-            int cnt = 0;
-            for (int i = 0; i < chArr.Length - 1; i++)
-            {
-                try 
-                {if (Convert.ToInt32(chArr[i]) > 0) cnt = cnt + 1;}
-                catch(IOException e)
-                {
-                    break;
-                }
-                
-            }
-            Console.WriteLine($"Положительных чисел введено {cnt} штук");
+string[] chArr = chisla.Split();
+int cnt = 0;
+for (int i = 0; i < chArr.Length - 1; i++)
+{
+    try
+    { if (Convert.ToInt32(chArr[i]) > 0) cnt = cnt + 1; }
+    catch (IOException e)
+    {
+        break;
+    }
+}
+// 1 - штука
+// 2,3,4 - штуки
+// 5-20 штук
+if (cnt % 10 == 1 && cnt !=11 ) {Console.WriteLine($"Положительных чисел введено {cnt} штука");}
+else if (cnt % 10 == 2 || cnt % 10 == 3 || cnt % 10 == 4) { Console.WriteLine($"Положительных чисел введено {cnt} штуки"); }
+else {Console.WriteLine($"Положительных чисел введено {cnt} штук");}
+
 
 // int Num = 0;
 // int i = 0;
