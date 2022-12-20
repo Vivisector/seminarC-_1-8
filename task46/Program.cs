c1ef5ad8959951069328e1fdf1511eba69152905
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-20 21:46:10 +0300

    task46 сборка и вывод двумерного массива

---
+++ task46/Program.cs
@@ -0,0 +1,37 @@
+﻿// Заполнить двумерный массив случайными числами
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int [,] GetRangeMatrix(int rows, int cols, int L = 0, int R = 10)
+{
+ Random rand = new Random();
+    int[,] matr = new int[rows, cols];
+    for(int i=0; i< matr.GetLength(0); i++)
+    {
+        for (int j = 0; j< matr.GetLength(1); j++)
+        {
+            matr[i,j]=rand.Next(L,R);
+        }
+    }
+return matr;
+}
+// метод печати двумерного массива
+void PrintMatrix(int [,] matr)
+{
+for (int i=0; i<matr.GetLength(0); i++)
+{
+    for (int j=0; j < matr.GetLength(1); j++)
+    {
+        Console.Write(matr[i,j] + " ");
+    }
+Console.WriteLine();
+}
+}
+
+int rowsCount = ReadInt("Введите число строк");
+int colsCount = ReadInt("Введите число колонок");
+int[,] matrix = GetRangeMatrix(rowsCount, colsCount);
+Console.WriteLine();
+PrintMatrix(matrix);
