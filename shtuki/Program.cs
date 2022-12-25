96def4178a1545e29a8d87e8ed08ad2224474bdc
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-25 12:19:47 +0300

    программка для выставления правильных падежей

---
+++ shtuki/Program.cs
@@ -0,0 +1,12 @@
+﻿string Shtuki(int count)
+{
+    string shtuk = string.Empty;
+if ((count % 10 == 1 && count != 11) || count == 0) shtuk =" раз";
+else if ((count % 10 == 2 || count % 10 == 3 || count % 10 == 4) && (count != 12 && count != 13 && count != 14)) shtuk = " раза";
+else shtuk = " раз";
+return shtuk;
+}
+Console.WriteLine("Введите любое число:");
+int chislo = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(chislo + Shtuki(chislo));
+Console.WriteLine(chislo + Shtuki(chislo));
