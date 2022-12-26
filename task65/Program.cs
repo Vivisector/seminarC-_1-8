8f1e545b09bf0b82965a63489f454b23f7a8306e
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-26 14:07:06 +0300

    task65 recursion summator zifr

---
+++ task65/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int M = 4; int N = 8;
+
+int LineUp(int m, int n)
+{
+    if (n == 1) return 1;
+    if (n == m) return m;
+    Console.Write(LineUp(m, n - 1) + " ");
+    return n;
+}
+Console.WriteLine(LineUp(M, N));
