d0537046b2651723143f8cbb793e845f0df590cc
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-26 22:01:01 +0300

    aboutRecursia - сделал наборчик факториалов от 1 до введенного числа для наглядного представления скорости возрастания результата

---
+++ aboutRecursia/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число до 16, иначе будет переполнение. Машина посчитает все факториалы в ряду от 1 до введенного числа.");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Factor(int n)
+{
+ if (n==0) return 1;
+ return n*Factor(n-1);   
+}
+for (int i=1; i<N+1; i++) Console.WriteLine($"Факториал от {i} равен {Factor(i)}");
