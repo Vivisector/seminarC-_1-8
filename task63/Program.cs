869b182a25b1960a495c608ccfc3fe762f082ddc
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-26 18:19:56 +0300

    task67 -figures summator

---
+++ task63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int N = 9;
+
+int GetLine(int n)
+{
+    if (n == 1) return n;
+   Console.Write(GetLine(n - 1) + " ");
+    return n;
+}
+
+Console.WriteLine(GetLine(N));
---
+++ task67/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int number = 45;
+int Summator(int n)
+{
+    if (n==0) return 0;
+    return n%10 + Summator(n/10);
+
+}
+Console.WriteLine(Summator(number));
