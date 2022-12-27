2574ba69e4c0d4c43784cb8122ae61ce05079cdd
Dmitry Beliakov <hulk295@yandex.ru>
2022-12-27 09:58:57 +0300

    task64 figures down line

---
+++ task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = 5;
+
+string LineBack(int n)
+{
+if (n >= 1) return $"{n} " + LineBack(n-1);
+else return string.Empty;
+}
+
+Console.WriteLine(LineBack(number));
