c6c4ccf4d92122a506da73e6fa033b54b7df2096
Dmitry Beliakov <hulk295@yandex.ru>
2023-01-02 19:47:31 +0300

    исправлена ошибка при выводе новой матрица. Вывод через строку

--- task59shorter/Program.cs
+++ task59shorter/Program.cs
@@ -1,4 +1,5 @@
-﻿int[,] matr = new int[4, 4];
+﻿int[,] matr = new int[6, 6];
+/////////////////////////////////////////////
 int[,] FillArr(int[,] m)
 {
     Random rand = new Random();
@@ -6,12 +7,12 @@ int[,] FillArr(int[,] m)
     {
         for (int j = 0; j < m.GetLength(1); j++)
         {
-            m[i, j] = rand.Next(0, 10);
+            m[i, j] = rand.Next(0, 100);
         }
     }
     return m;
 }
-
+/////////////////////////////////////////////
 void ReadMatr(int[,] m)
 {
     for (int i = 0; i < m.GetLength(0); i++)
@@ -23,7 +24,7 @@ void ReadMatr(int[,] m)
         Console.WriteLine();
     }
 }
-
+/////////////////////////////////////////////
 (int, int) GetMin(int[,] m)
 {
     int min = m[0, 0]; int posMin1 = 0; int posMin2 = 0;
@@ -41,8 +42,8 @@ void ReadMatr(int[,] m)
     }
     return (posMin1, posMin2);
 }
-
-int[,] Rearrange(int[,] m, int p1, int p2)
+/////////////////////////////////////////////
+string Rearrange(int[,] m, int p1, int p2)
 {
     int[,] rearr = new int[m.GetLength(0) - 1, m.GetLength(1) - 1];
     string s = string.Empty;
@@ -52,23 +53,24 @@ int[,] Rearrange(int[,] m, int p1, int p2)
         {
             for (int j = 0; j < m.GetLength(1); j++)
             {
-                if (j != p2) 
+                if (j != p2)
                 {
-                    if (j>p2 && i>p1) rearr[i-1,j-1]=m[i,j];
-                    else if (i>p1) rearr[i-1,j]=m[i,j];
-                    else if (j>p2) rearr[i,j-1]=m[i,j];
+                    s = s + m[i, j] + " ";
                 }
-            }             
+            }
+            s = s + "\n";
+            // string[] ss = s.Split(); s = string.Empty; Console.WriteLine("!" + ss.Length);
+            // for (int k=0; k<ss.Length-1; k++){rearr[i,k] = Convert.ToInt32(ss[k]);}
         }
     }
-    return rearr;
+    return s;
 }
-
+/////////////////////////////////////////////
 int[,] m = FillArr(matr);
 Console.WriteLine("исходная матрица:");
 ReadMatr(m);
 Console.WriteLine($"минимальный элемент ({m[GetMin(m).Item1, GetMin(m).Item2]}) находится в {GetMin(m).Item1 + 1} строке, в {GetMin(m).Item2 + 1} колонке");
 Console.WriteLine("После удаления этого ряда и колонки матрица принимает вид:");
 int pos1 = GetMin(m).Item1; int pos2 = GetMin(m).Item2;
-int[,] m2 = Rearrange(m, pos1, pos2);
-ReadMatr(m2);
+string m2 = Rearrange(m, pos1, pos2);
+Console.WriteLine(m2);
